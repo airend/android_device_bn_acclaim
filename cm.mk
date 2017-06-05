belfastraven/android_device_bn_common

releasetools: Resize system partition after flashing
Some people enlarge system partition to fit more than pico GApps.
Flash a stock sized partition, and resize to the actual partition
size before restoring GApps.

Signed-off-by: David R. Marble <drmarble1@gmail.com>
Change-Id: Ib481f39632a873fc741f2d212aefea3f8a89dd93
Lineage-only for now, since it uses custom plumbing
Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

cm.mk
@@ -12,5 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEVICE_PACKAGE_OVERLAYS += device/bn/common/overlay/cm
+COMMON_FOLDER := device/bn/common
 
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_FOLDER)/overlay/cm
+
+TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_FOLDER)/recovery/extensions
+
+# Device & filesystem
+PRODUCT_PACKAGES += \
+    resize2fs_static

airend/android_device_bn_acclaim

cm.mk
@@ -13,14 +13,21 @@
 # limitations under the License.
 
 # Inherit device configuration for acclaim
+DEVICE_FOLDER := device/bn/acclaim
 
-$(call inherit-product, device/bn/acclaim/full_acclaim.mk)
+$(call inherit-product, $(DEVICE_FOLDER)/full_acclaim.mk)
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
 TARGET_SCREEN_WIDTH := 480
 TARGET_SCREEN_HEIGHT := 1024
 TARGET_BOOTANIMATION_HALF_RES := true
 
-DEVICE_PACKAGE_OVERLAYS += device/bn/acclaim/overlay/cm
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_FOLDER)/overlay/cm
+
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_FOLDER)/recovery/extensions
+
+# Device & filesystem
+PRODUCT_PACKAGES += \
+    resize2fs_static
 
 PRODUCT_NAME := cm_acclaim
 PRODUCT_DEVICE := acclaim